08e855dea97aaa6e69dccf99ba9a4efadc92e6ba
naimkst <naimkst@gmail.com>
2024-09-27 23:13:10 +0600

    update to server

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@ module.exports = () => ({
     enabled: true,
     config: {
       apiVersion: "2024-07", // Or one of Shopify's supported API versions
-      accessToken: "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", // The environment variable containing your private app's access token
+      accessToken: "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", // The environment variable containing your private app's access token
       shopName: "finishsense.myshopify.com", // The environment variable containing your myshopify.com domain
     },
   },
